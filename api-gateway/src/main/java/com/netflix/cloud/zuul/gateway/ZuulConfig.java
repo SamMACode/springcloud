75c96f499f7f0d4ac9d575c53c29109ae3db810e
Sam Ma <sam_newyork@163.com>
2018-10-06 10:46:47 +0800

    #zuul实现动态路由以及cookie,路由转发、排除和自定义

---
+++ api-gateway/src/main/java/com/netflix/cloud/zuul/gateway/ZuulConfig.java
@@ -0,0 +1,22 @@
+package com.netflix.cloud.zuul.gateway;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * zuul配置的动态注入
+ *
+ * @author dong
+ * @create 2018-10-06 上午10:42
+ **/
+@Component
+public class ZuulConfig {
+
+    @RefreshScope
+    @ConfigurationProperties("zuul")
+    public ZuulProperties zuulProperties() {
+        return new ZuulProperties();
+    }
+}
