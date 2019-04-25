88311578a56837f4e7d80ba86402d6c6370ed323
SamMaCode <sam_newyork@163.com>
2019-04-26 00:43:32 +0800

    refactor micro-order-service & micro-product-service

---
+++ order/micro-order-service/src/main/java/com/netflix/cloud/order/vo/ResultVOUtils.java
@@ -0,0 +1,18 @@
+package com.netflix.cloud.order.vo;
+
+/**
+ * 用于封装最后处理的结果信息
+ *
+ * @author dong
+ * @create 2018-09-26 下午10:53
+ **/
+public class ResultVOUtils {
+
+    public static ResultVO success(Object object) {
+        ResultVO resultVO = new ResultVO();
+        resultVO.setCode(0);
+        resultVO.setMessage("成功");
+        resultVO.setData(object);
+        return resultVO;
+    }
+}
